aa10aa80383a528b52c42e1fe471b37df3a5f466
cecillaka <ceciltebogolaka@gmail.com>
2020-12-14 14:00:21 +0200

    hope's commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ContentComponent } from './content/content.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+
+  {path: 'register' , component: RegisterComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'content', component: ContentComponent},
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
